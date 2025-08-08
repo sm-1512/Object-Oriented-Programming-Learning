73e20382c9f023e9de949d92fe23e9dd328d0dc9
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-08-08 22:25:32 +0530

    Types of Constructors

--- constructor.cpp
+++ constructor.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Teacher{
 public:
+    //Non-parameterised
     Teacher(){
         //This is a constructor function.
         //Same name as class.
@@ -13,14 +14,31 @@ public:
 
         subject = "Computer Science";
     }
+
+    //Parameterised Constructor
+    Teacher(string nm, string d, string sub){
+        name = nm;
+        dept = d;
+        subject = sub;
+    }
     string name;
     string dept;
     string subject;
+
+
+    void getInfo(){
+        cout<< "Name :" << name << endl;
+        cout<< "Department :" << dept << endl;
+        cout<< "Subject :" << subject << endl;
+    }
 };
 
 
 
 int main(){
-    Teacher t1; //Constructor call
-    cout<<t1.subject<<endl;
+    //Teacher t1; Constructor call
+    //cout<<t1.subject<<endl;
+
+    Teacher t1("John Doe", "Computer Science", "C++"); //Calling parameteried constructor
+    t1.getInfo();
 }
